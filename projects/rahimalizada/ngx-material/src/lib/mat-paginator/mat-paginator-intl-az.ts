802931ad41ced0a483b39d94a67b9cdd919719d6
Rahim Alizada <rahim.alizada@gmail.com>
2020-09-22 15:37:27 +0400

    Fixed a typo in MatPaginatorIntlAz

--- projects/rahimalizada/ngx-material/src/lib/mat-paginator/mat-paginator-intl-az.ts
+++ projects/rahimalizada/ngx-material/src/lib/mat-paginator/mat-paginator-intl-az.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 
 @Injectable()
-export class MatPaginatorIntlAze extends MatPaginatorIntl {
+export class MatPaginatorIntlAz extends MatPaginatorIntl {
   itemsPerPageLabel = 'Sıra sayı';
   nextPageLabel = 'Növbəti səhifə';
   previousPageLabel = 'Əvvəlki səhifə';
