6099f8eb90bc5adcf4a2d86713016a17dcc1add2
Rahim Alizada <rahim.alizada@gmail.com>
2021-05-14 23:13:34 +0400

    Updated build script

--- .patch-version-and-publish.cmd
+++ .patch-version-and-publish.cmd
@@ -8,4 +8,7 @@ call npm version patch --force
 git push origin master
 git push origin --tags
 
+call ng build @rahimalizada/%name% --prod
+call npm publish .\dist\rahimalizada\%name%\ --access public
+
 pause
